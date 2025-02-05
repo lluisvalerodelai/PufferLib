9d9118cd28883379227c3a167b0b0c31d30e95e4
Luis Valero <lluisvalerodelai@gmail.com>
2025-02-05 09:33:47 +0100

    flappy bird: collission checking

--- pufferlib/ocean/flappy_bird/flappy_bird.c
+++ pufferlib/ocean/flappy_bird/flappy_bird.c
@@ -1,11 +1,13 @@
 #include "flappy_bird.h"
 #include "raylib.h"
 
-
 int main(void) {
 
+  bool done = false;
   InitWindow(WIDTH, HEIGHT, "Flappy bird");
   SetTargetFPS(60);
+  unsigned int score = 0;
+  char str[32];
 
   Bird *bird = generateBird();
   PipePair *pipes[num_pipes_screen];
@@ -20,7 +22,11 @@ int main(void) {
 
     BeginDrawing();
 
-    DrawCircle(bird->x_pos, bird->y_pos, 15, RED);
+    char* score_str = uint_to_str(score);
+    DrawText(score_str, 0, 0, 50, WHITE);
+    free(score_str);
+
+    DrawCircle(bird->x_pos, bird->y_pos, bird->radius, RED);
     renderPipes(pipes, 0);
 
     ClearBackground(SKYBLUE);
@@ -34,17 +40,23 @@ int main(void) {
       renderPipes(pipes, 1);
     }
 
-    if (pipes[1]->gapX < bird->x_pos) {
+    if (pipes[1]->gapX + pipes[1]->gapWidth < bird->x_pos) {
       renderPipes(pipes, 2);
       pipes[0] = pipes[1];
       pipes[1] = pipes[2];
       pipes[2] = new_pipe();
     }
 
+    if (checkCollisions(bird, pipes[1])) {
+      done = true;
+      break;
+    } else {
+      score += 1;
+    }
   }
 
   CloseWindow();
   // free everything
-
+  
   return 0;
 }
--- pufferlib/ocean/flappy_bird/flappy_bird.h
+++ pufferlib/ocean/flappy_bird/flappy_bird.h
@@ -1,5 +1,5 @@
-#include <raylib.h>
 #include "raylib.h"
+#include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +15,7 @@ typedef struct {
   int x_pos;
   int vertical_velocity;
   int gravity;
+  int radius;
 } Bird;
 
 typedef struct {
@@ -29,33 +30,70 @@ Bird *generateBird() {
   Bird *bird = (Bird *)calloc(1, sizeof(Bird));
   bird->y_pos = 200;
   bird->x_pos = 200;
+  bird->radius = 15;
 
   return bird;
 }
 
 PipePair *new_pipe() {
-    PipePair *pipes = (PipePair *)calloc(1, sizeof(PipePair));
+  PipePair *pipes = (PipePair *)calloc(1, sizeof(PipePair));
 
-    pipes->velocity = 1;
-    pipes->gapY = 100 + (rand() % 300);
-    pipes->gapX = WIDTH;
-    pipes->gapSize = 125 + ((rand() % 75));
-    pipes->gapWidth = 100;
+  pipes->velocity = 1;
+  pipes->gapY = 100 + (rand() % 300);
+  pipes->gapX = WIDTH;
+  pipes->gapSize = 125 + ((rand() % 75));
+  pipes->gapWidth = 100;
 
   return pipes;
 }
 
+// setPipeList sets the initial 2 pipes, afterwards new pipes are generated one
+// by one
 void setPipeList(PipePair *pipes[num_pipes_screen]) {
-  for (int i = 0; i < num_pipes_screen; i++) {
+  pipes[0] = (PipePair *)calloc(1, sizeof(PipePair));
+  for (int i = 1; i < num_pipes_screen; i++) {
     pipes[i] = new_pipe();
   }
 }
 
 void renderPipes(PipePair *pipes[num_pipes_screen], int pipeNum) {
 
-  DrawRectangle(pipes[pipeNum]->gapX, pipeNum, pipes[pipeNum]->gapWidth, pipes[pipeNum]->gapY, GREEN);
-  DrawRectangle(pipes[pipeNum]->gapX, pipes[pipeNum]->gapY + pipes[pipeNum]->gapSize, pipes[pipeNum]->gapWidth,
+  DrawRectangle(pipes[pipeNum]->gapX, 0, pipes[pipeNum]->gapWidth,
+                pipes[pipeNum]->gapY, GREEN);
+  DrawRectangle(pipes[pipeNum]->gapX,
+                pipes[pipeNum]->gapY + pipes[pipeNum]->gapSize,
+                pipes[pipeNum]->gapWidth,
                 HEIGHT - pipes[pipeNum]->gapY + pipes[pipeNum]->gapSize, GREEN);
 
   pipes[pipeNum]->gapX -= pipes[pipeNum]->velocity;
+}
+
+bool checkCollisions(Bird *bird, PipePair *pipes) {
+
+  if (bird->y_pos > HEIGHT)
+    return true;
+
+
+  //check collision with the top pipe
+  bool collisionY = bird->y_pos - bird->radius < pipes->gapY;
+  bool collisionX = bird->x_pos + bird->radius > pipes->gapX;
+
+  if (collisionX && collisionY)
+    return true;
+
+  //check collision for the bottom poipe
+  collisionY = bird->y_pos + bird->radius > pipes->gapY + pipes->gapSize;
+
+  if (collisionY && collisionX) return true;
+
+  return false;
+}
+
+char* uint_to_str(unsigned int num) {
+    // Max length for an unsigned int string (assuming 32-bit) + null terminator
+    char* str = (char *) malloc(11); 
+    if (!str) return NULL;  // Check for allocation failure
+
+    sprintf(str, "%u", num);
+    return str;  // Caller must free this memory
 }
