6abcf556a0eab1b1b9675c785de6597c8eea89b1
Luis Valero <lluisvalerodelai@gmail.com>
2025-02-06 09:00:27 +0100

    flappy bird C env finished

--- pufferlib/ocean/flappy_bird/flappy_bird.c
+++ pufferlib/ocean/flappy_bird/flappy_bird.c
@@ -1,60 +1,40 @@
 #include "flappy_bird.h"
+#include "../../puffernet.h"
 #include "raylib.h"
 
 int main(void) {
 
-  bool done = false;
-  InitWindow(WIDTH, HEIGHT, "Flappy bird");
-  SetTargetFPS(60);
-  unsigned int score = 0;
-  char str[32];
+  // Weights* weights = load_weights("resources/pong_weights.bin", 133764);
+  // LinearLSTM* net = make_linearlstm(weights, 1, 8, 3);
 
   Flappy_client *client = generate_client();
+  Flappy_env *env = generateEnv();
+
+  InitWindow(WIDTH, HEIGHT, "Flappy bird");
+  SetTargetFPS(60);
+
+  //c_reset(env)
 
   while (!WindowShouldClose()) {
 
     if (GetKeyPressed() == KEY_SPACE) {
-      client->bird.vertical_velocity = -10;
-    }
-    client->bird.y_pos += client->bird.vertical_velocity;
-
-    BeginDrawing();
-
-    char* score_str = uint_to_str(score);
-    DrawText(score_str, 0, 0, 50, WHITE);
-    free(score_str);
-
-    DrawCircle(client->bird.x_pos, client->bird.y_pos, client->bird.radius, RED);
-    renderPipes(client->pipes, 0);
-
-    ClearBackground(SKYBLUE);
-    EndDrawing();
-
-    if (client->bird.vertical_velocity < 20) {
-      client->bird.vertical_velocity += gravity;
-    }
-
-    if (client->pipes[0].gapX < client->bird.x_pos) {
-      renderPipes(client->pipes, 1);
-    }
-
-    if (client->pipes[1].gapX + client->pipes[1].gapWidth < client->bird.x_pos) {
-      renderPipes(client->pipes, 2);
-      client->pipes[0] = client->pipes[1];
-      client->pipes[1] = client->pipes[2];
-      client->pipes[2] = new_pipe();
-    }
-
-    if (checkCollisions(client->bird, client->pipes[1])) {
-      done = true;
-      break;
+      env->action = JUMP;
     } else {
-      score += 1;
+      env->action = NOOP;
+    }
+
+    c_step(env, client);
+    c_render(env, client);
+
+    if (env->done) {
+      break;
     }
   }
 
   CloseWindow();
   // free everything
-  
+  free(env);
+  free(client);
+
   return 0;
 }
--- pufferlib/ocean/flappy_bird/flappy_bird.h
+++ pufferlib/ocean/flappy_bird/flappy_bird.h
@@ -6,6 +6,8 @@
 #define WIDTH 500
 #define HEIGHT 800
 #define PIPE_SEPARATION 200
+#define NOOP 0
+#define JUMP 1
 
 #define num_pipes_screen 3
 
@@ -35,18 +37,23 @@ typedef struct {
 } Flappy_env;
 
 typedef struct {
-  Bird bird;  
+  Bird *bird;
   PipePair pipes[num_pipes_screen];
 
 } Flappy_client;
 
-Bird generateBird() {
+Flappy_env *generateEnv() {
+  Flappy_env *env = (Flappy_env *)calloc(1, sizeof(Flappy_env));
+  return env;
+}
+
+Bird *generateBird() {
   Bird *bird = (Bird *)calloc(1, sizeof(Bird));
-  bird->y_pos = 200;
+  bird->y_pos = 0;
   bird->x_pos = 200;
   bird->radius = 15;
 
-  return *bird;
+  return bird;
 }
 
 PipePair new_pipe() {
@@ -64,7 +71,8 @@ PipePair new_pipe() {
 // setPipeList sets the initial 2 pipes, afterwards new pipes are generated one
 // by one
 void setPipeList(Flappy_client *client) {
-  client->pipes[0] = *(PipePair *)calloc(1, sizeof(PipePair));
+  client->pipes[0] =
+      *(PipePair *)calloc(1, sizeof(PipePair)); // we dont render pipe 0
   for (int i = 1; i < num_pipes_screen; i++) {
     client->pipes[i] = new_pipe();
   }
@@ -91,32 +99,88 @@ void renderPipes(PipePair pipes[num_pipes_screen], int pipeNum) {
   pipes[pipeNum].gapX -= pipes[pipeNum].velocity;
 }
 
-bool checkCollisions(Bird bird, PipePair pipes) {
+bool checkCollisions(Bird *bird, PipePair *pipes) {
 
-  if (bird.y_pos > HEIGHT)
+  if (bird->y_pos > HEIGHT)
     return true;
 
-
-  //check collision with the top pipe
-  bool collisionY = bird.y_pos - bird.radius < pipes.gapY;
-  bool collisionX = bird.x_pos + bird.radius > pipes.gapX;
+  // check collision with the top pipe
+  bool collisionY = bird->y_pos - bird->radius < pipes->gapY;
+  bool collisionX = bird->x_pos + bird->radius > pipes->gapX;
 
   if (collisionX && collisionY)
     return true;
 
-  //check collision for the bottom poipe
-  collisionY = bird.y_pos + bird.radius > pipes.gapY + pipes.gapSize;
+  // check collision for the bottom poipe
+  collisionY = bird->y_pos + bird->radius > pipes->gapY + pipes->gapSize;
 
-  if (collisionY && collisionX) return true;
+  if (collisionY && collisionX)
+    return true;
 
   return false;
 }
 
-char* uint_to_str(unsigned int num) {
-    // Max length for an unsigned int string (assuming 32-bit) + null terminator
-    char* str = (char *) malloc(11); 
-    if (!str) return NULL;  // Check for allocation failure
+// for rendering the score on the screen
+char *uint_to_str(unsigned int num) {
+  // Max length for an unsigned int string (assuming 32-bit) + null terminator
+  char *str = (char *)malloc(11);
 
-    sprintf(str, "%u", num);
-    return str;  // Caller must free this memory
+  sprintf(str, "%u", num);
+  return str; // Caller must free this memory
 }
+
+void c_step(Flappy_env *env, Flappy_client *client) {
+
+  // update velocities
+  if (env->action == JUMP) {
+    client->bird->vertical_velocity = -10;
+  }
+
+  client->bird->y_pos += client->bird->vertical_velocity;
+
+  if (client->bird->vertical_velocity < 20) {
+    client->bird->vertical_velocity += gravity;
+  }
+
+  // update the pipe render order if needed
+  if (client->pipes[1].gapX + client->pipes[1].gapWidth < client->bird->x_pos) {
+    client->pipes[0] = client->pipes[1];
+    client->pipes[1] = client->pipes[2];
+    client->pipes[2] = new_pipe();
+  }
+
+  // check if env is done
+  if (checkCollisions(client->bird, &client->pipes[1])) {
+    env->done = true;
+  } else {
+    env->reward += 1;
+  }
+}
+
+void c_render(Flappy_env *env, Flappy_client *client) {
+  BeginDrawing();
+
+  DrawCircle(client->bird->x_pos, client->bird->y_pos, client->bird->radius,
+             RED);
+
+  renderPipes(client->pipes, 0);
+
+  if (client->pipes[0].gapX < client->bird->x_pos) {
+    renderPipes(client->pipes, 1);
+  }
+
+  if (client->pipes[1].gapX + client->pipes[1].gapWidth < client->bird->x_pos) {
+    renderPipes(client->pipes, 2);
+  }
+
+  char *score_str = uint_to_str(env->reward);
+  DrawText(score_str, 0, 0, 50, WHITE);
+  free(score_str);
+
+  ClearBackground(SKYBLUE);
+  EndDrawing();
+}
+
+void free_env(Flappy_env *env) { free(env); }
+
+void free_client(Flappy_client *client) { free(client); }
