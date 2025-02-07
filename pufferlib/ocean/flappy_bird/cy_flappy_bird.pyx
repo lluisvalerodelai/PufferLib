1043b9dc5387edb03a9a4d1cac5617fdf5d0edf7
Luis Valero <lluisvalerodelai@gmail.com>
2025-02-07 21:30:05 +0100

    fix collision detection from refactor

--- pufferlib/ocean/flappy_bird/cy_flappy_bird.pyx
+++ pufferlib/ocean/flappy_bird/cy_flappy_bird.pyx
@@ -1,48 +1,17 @@
 cdef extern from "flappy_bird.h":
-    int WIDTH
-    int HEIGHT
-    int PIPE_SEPARATION
-    int NOOP
-    int JUMP
-    int gravity
-    int num_pipes_screen
 
-    ctypedef struct Bird:
-        int y_pos
-        int x_pos
-        int vertical_velocity
-        int gravity
-        int radius
-
-    ctypedef struct PipePair:
-        int velocity
-        int gapY
-        int gapX
-        int gapSize
-        int gapWidth
-    
     ctypedef struct Flappy_env:
-        int state[4]
-        int action
-        int reward
-        bint done
+        int *state[4];
+        Bird *bird;
+        PipePair *pipes[3];
+        int action;
+        int reward;
+        bool done;
+
     
     ctypedef struct Flappy_client:
-        Bird *bird
-        PipePair pipes[3] #change to num_pipes_screen
-
-    Flappy_env *generateEnv()
-    Bird *generateBird()
-    PipePair new_pipe()
-    void setPipeList(Flappy_client *client)
-    Flappy_client *generate_client()
-    void renderPipes(PipePair *pipes[3], int pipeNum) #also change to num_pipes_screen
-    bint checkCollisions(Bird *bird, PipePair*pipes)
-    char *uint_to_str(unsigned int num)
-    void c_step(Flappy_env *env, Flappy_client *client)
-    void c_render(Flappy_env *env, Flappy_client *client) 
-    void free_env(Flappy_env *env)
-    void free_client(Flappy_client *client)
+      int WIDTH;
+      int HEIGHT;
 
 cdef class CyFlappyBird:
     cdef:
@@ -50,7 +19,7 @@ cdef class CyFlappyBird:
         Flappy_env *env
 
     def __init__(self):
-        self.env = generateEnv()
+        self.env = Flappy_env()
         self.client = generate_client()
 
     def step(self, int action):
